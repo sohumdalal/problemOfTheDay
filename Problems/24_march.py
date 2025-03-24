2ee58047d2db4062ed8f9ae44db6b2a71fab61f9
sohumdalal <dsohum@gmail.com>
2025-03-23 21:52:54 -0400

    3/24

---
+++ Problems/24_march.py
@@ -0,0 +1,23 @@
+'''
+Problem Of The Day - 3/24/2025
+
+Doubly Linked List
+
+Implement a Doubly Linked List (DLL) in Python. Your DLL should have the following features:
+
+Class: Node – please include the following attributes:
+--- data: The value stored in the node.
+--- prev: A reference to the previous node.
+--- next: A reference to the next node.
+
+Class: DoublyLinkedList – please include and test the following methods:
+--- append(data): Adds a node with the given data to the end of the list.
+--- prepend(data): Adds a node with the given data to the beginning of the list.
+--- delete(data): Removes the first occurrence of a node with the given data.
+--- search(data): Returns True if the data is found, otherwise False.
+--- display_forward(): Prints the elements from head to tail.
+--- display_backward(): Prints the elements from tail to head.
+
+'''
+
+

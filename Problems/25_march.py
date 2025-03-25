d49ee0371d00e698ea4cc78f4c78f647ee201c8e
sohumdalal <dsohum@gmail.com>
2025-03-25 08:58:25 -0400

    adding POD for 3/25

---
+++ Problems/25_march.py
@@ -0,0 +1,31 @@
+'''
+Problem Of The Day - 3/24/2025
+
+Doubly Linked List
+
+Implement a Doubly Linked List (DLL) in Python. Your DLL should have the following features:
+
+Class: Node – please include the following attributes:
+--- data: The value stored in the node.
+--- prev: A reference to the previous node.
+--- next: A reference to the next node.
+
+Class: DoublyLinkedList – please include and test the following methods:
+--- append(data): Adds a node with the given data to the end of the list.
+--- prepend(data): Adds a node with the given data to the beginning of the list.
+--- delete(data): Removes the first occurrence of a node with the given data.
+--- search(data): Returns True if the data is found, otherwise False.
+--- display_forward(): Prints the elements from head to tail.
+--- display_backward(): Prints the elements from tail to head.
+
+Sorting a Doubly Linked List (3/25)
+
+Implement a sort() method that sorts the doubly linked list in ascending order.
+Try to do this in-place without creating a new list.
+Hint: You can use Insertion Sort since it works well with linked lists.
+
+'''
+
+
+
+

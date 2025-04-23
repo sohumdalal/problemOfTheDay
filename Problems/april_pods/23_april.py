8b39f60385b5e6642372f00744e92f9aff6ba4ed
sohumdalal <dsohum@gmail.com>
2025-04-23 13:09:11 -0400

    attempt to flatten 4.23

--- Problems/april_pods/23_april.py
+++ Problems/april_pods/23_april.py
@@ -9,3 +9,19 @@ Output: [1, 2, 3, 4, 5, 6]
 Can you use recursion?
 
 '''
+
+def flatten(array, index = 0, final = []):
+    if not isinstance(array, list):
+        print("array", array)
+        return final
+    else:
+        if isinstance(array[index], list):
+            flatten(array[index], 0, final)
+        else:
+            print("final:", final)
+            final.append(array[index])
+            flatten(array[index + 1], index + 1, final)
+                 
+
+print(flatten([1, [2, [3, 4], 5], 6]))
+
